63904037804780a3aeb4dbea14fe13eba8f3b718
Agnes Bertua Nababan <agnesnababan50@gmail.com>
2022-12-02 10:27:32 +0700

    add OAuth

--- src/views/User/login.js
+++ src/views/User/login.js
@@ -1,16 +1,38 @@
-import React from 'react'
-import { Button, Form, Container, Row, Col } from 'react-bootstrap'
+import React, { useEffect } from 'react'
+import { Button, Form, Container, Row } from 'react-bootstrap'
 import '../../styles/login.css'
 import account from '../../assets/account.svg'
+import GoogleComponent from 'react-google-login'
+import { gapi } from 'gapi-script'
 
-const login = () => {
+function login () {
+  const responseGoogle = (response) => {
+    // console.log(response);
+    window.localStorage.setItem('accessToken', response.accessToken)
+  }
+  useEffect(() => {
+    function start () {
+      gapi.client.init({
+        clientId:
+                    '267971687256-iphrhvg80ireilb73gjjnvvcg0fdavu2.apps.googleusercontent.com',
+        scope: ''
+      })
+    }
+
+    gapi.load('client:auth2', start)
+  }, [])
   return (
         <Container>
             <Row className="justify-content-center m-auto">
                 <div id="login">
                     <div id="sign-in">
-                        <Col md={{ span: 8, offset: 5 }}><img src={account} className="m-md-2" /></Col>
-                        <h3>MASUK</h3>
+                        <div className="d-grid gap-8 justify-content-center">
+                            <img src={account} />
+                        </div>
+                        <div className="d-grid gap-8 justify-content-center mt-2">
+                            <h3>MASUK</h3>
+                        </div>
+
                         <Form >
                             <Form.Group className="mt-5 mb-3 shadow-sm" controlId="formBasicEmail">
                                 <Form.Control type="email" placeholder="Username" />
@@ -32,6 +54,16 @@ const login = () => {
                                 <p className="mt-1"> Or Sign in with :</p>
                             </div>
 
+                            <div className="d-grid gap-8 justify-content-center">
+                                <GoogleComponent
+                                    clientId="267971687256-iphrhvg80ireilb73gjjnvvcg0fdavu2.apps.googleusercontent.com"
+                                    onSuccess={responseGoogle}
+                                    onFailure={responseGoogle}
+                                    cookiePolicy={'single_host_origin'}
+                                    className="m-md-3"
+                                />
+                            </div>
+
                         </Form>
                     </div>
                 </div>
@@ -40,24 +72,3 @@ const login = () => {
   )
 }
 export default login
-/* // <div id="login">
-        //     <div id="sign-in">
-        //         <
-        //         <h3>SIGN IN TO (APPLICATION NAME)</h3>
-        //         <Form>
-        //             <Form.Group className="mb-3" controlId="formBasicEmail">
-        //                 <Form.Control type="email" placeholder="Enter email" />
-        //             </Form.Group>
-
-        //             <Form.Group className="mb-3" controlId="formBasicPassword">
-        //                 <Form.Control type="password" placeholder="Password" />
-        //             </Form.Group>
-        //             <Button variant="primary" type="submit">
-        //                 Sign-in
-        //             </Button>
-
-        //             <p className="mt-3">Dont Have an Account ?  <a href="/sign-up">Sing up</a></p>
-
-        //         </Form>
-        //     </div>
-        // </div> */
