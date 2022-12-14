d95a76a565d42fc94b5dc69fd72eed802093654d
afanfian <afanfian14@gmail.com>
2022-12-14 20:08:49 +0700

    fix: update desain halaman 404 not found

--- src/views/notFoundPage.jsx
+++ src/views/notFoundPage.jsx
@@ -1,32 +1,34 @@
 import React from 'react'
 import '../styles/notFoundPage.css'
+import logo from '../assets/Home/logo-2.svg'
 
 function NotFoundPage () {
   return (
-      <div className="container vh-100 d-flex align-items-center justify-content-center">
-        <div className="row">
-          <div className="col-md-12">
-            <div className="error-template">
-              <h1>
-                Oops!
-              </h1>
-              <h2>
-                404 Not Found
-              </h2>
-              <div className="error-details">
-                Sorry, an error has occured, Requested page not found!
-              </div>
-              <div className="error-actions">
-                <a href="/" >
-                  <button className="btn-green">
-                    Take Me Home
-                  </button>
-                </a>
-              </div>
+        <div className="container vh-100 d-flex align-items-center justify-content-center">
+            <div className="row">
+                <div className="col-md-12">
+                <div className="error-template">
+                    <img src={logo} alt="logo" width={150}/>
+                    <h1>
+                    Oops!
+                    </h1>
+                    <h2>
+                    404 Not Found
+                    </h2>
+                    <div className="error-details">
+                    Sorry, an error has occured, Requested page not found!
+                    </div>
+                    <div className="error-actions">
+                    <a href="/" >
+                        <button className="btn-green">
+                        Take Me Home
+                        </button>
+                    </a>
+                    </div>
+                </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
   )
 }
 
