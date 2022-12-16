5ae7336f3d3ccf4b3054fc757485dd4aeaca52e5
afanfian <afanfian14@gmail.com>
2022-12-16 08:21:41 +0700

    fix: rename file images

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,24 +4,24 @@ import VisionMision from './About/img-vision.png'
 // Image Admin
 
 // Image Home
-import Landing from './Home/airplane-landing.svg'
-import Takeoff from './Home/airplane-takeoff.svg'
-import Calendar from './Home/calendar-date.svg'
+import Landing from './Home/airplaneLanding.svg'
+import Takeoff from './Home/airplaneTakeoff.svg'
+import Calendar from './Home/calendarDate.svg'
 import Content1 from './Home/content1.png'
 import Content2 from './Home/content2.svg'
 import Content3 from './Home/content3.svg'
-import Facebook from './Home/icon_facebook.svg'
-import Instagram from './Home/icon_instagram.svg'
-import Email from './Home/icon_mail.svg'
+import Facebook from './Home/iconFacebook.svg'
+import Instagram from './Home/iconInstagram.svg'
+import Email from './Home/iconMail.svg'
 import Logo from './Home/logo2.svg'
 import Person from './Home/person.svg'
-import Sofa from './Home/sofa-single.svg'
-import LogoTicket from './Home/ticket-vocation.svg'
+import Sofa from './Home/sofaSingle.svg'
+import LogoTicket from './Home/ticketVocation.svg'
 
 // Image User
 import account from './User/account.svg'
 import gambarIcon from './User/Aircraft.png'
 import LogoPesawat from './User/airplane.svg'
-import LogoPesawatNavbar from './User/logo-pesawat.svg'
+import LogoPesawatNavbar from './User/logoPesawat.svg'
 
 export { Landing, Takeoff, Calendar, Facebook, Instagram, Email, Logo, Person, Sofa, LogoTicket, VisionMision, Content1, Content2, Content3, account, gambarIcon, LogoPesawatNavbar, LogoPesawat }
