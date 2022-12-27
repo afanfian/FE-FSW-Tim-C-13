1510ccc64b7988a65e43e04022330a203875b498
afanfian <afanfian14@gmail.com>
2022-12-27 20:01:45 +0700

    fix: update slicing halaman ticket list

---
+++ src/views/TicketList/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {TicketList} from '../../components'
+
+const Ticket = () => {
+  return (
+    <div>
+      <TicketList/>
+    </div>
+  )
+}
+
+export default Ticket
