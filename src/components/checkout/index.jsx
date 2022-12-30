8a57afe7ff5caec7c480fb4b16ff0e242b1a037f
afanfian <afanfian14@gmail.com>
2022-12-30 19:50:14 +0700

    fix: update styling checkout

--- src/components/checkout/index.jsx
+++ src/components/checkout/index.jsx
@@ -22,7 +22,7 @@ function checkout (){
                             <h3 className="fw-bold pb-2">Thank You for Your Order on <span className="text-green">C-13 AEROPLANE</span> !</h3>
                             <h3>Good Luck on Your Flight <span className="text-green">{passenger.passenger_name}</span></h3>
                         </div>
-                        <Card id="#card-bg" className="col-md-6">
+                        <Card id="card-bg" className="col-md-6">
                             <Card.Body>
                                 <p className="fw-bold text-center h4">[Your Flight Ticket]</p>
                                 <p className="fw-bold h5">Name :</p>
