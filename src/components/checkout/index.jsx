f5eca0766af215921e55f849a64d4afc2ab46590
afanfian <afanfian14@gmail.com>
2022-12-30 19:49:45 +0700

    fix: update styling checkout

--- src/components/checkout/index.jsx
+++ src/components/checkout/index.jsx
@@ -22,14 +22,16 @@ function checkout (){
                             <h3 className="fw-bold pb-2">Thank You for Your Order on <span className="text-green">C-13 AEROPLANE</span> !</h3>
                             <h3>Good Luck on Your Flight <span className="text-green">{passenger.passenger_name}</span></h3>
                         </div>
-                        <Card className="col-md-6">
+                        <Card id="#card-bg" className="col-md-6">
                             <Card.Body>
                                 <p className="fw-bold text-center h4">[Your Flight Ticket]</p>
-                                <p className="fw-bold h5">Name:</p>
+                                <p className="fw-bold h5">Name :</p>
                                 <p className="h6">{passenger.passenger_name}</p>
-                                <p className="fw-bold h5">Airport Name:</p>
+                                <p className="fw-bold h5">ID Number :</p>
+                                <p className="h6">{passenger.nik}</p>
+                                <p className="fw-bold h5">Airport Name :</p>
                                 <p className="h6">{airport.airport_name}</p>
-                                <p className="fw-bold h5">Departure Date:</p>
+                                <p className="fw-bold h5">Departure Date :</p>
                                 <p className="h6">{ticket.departure_date.split("T")[0]}</p>
                                 <p className="fw-bold h5">Price:</p>
                                 <p className="h6">{ticket.price}</p>
